8acae0596b899a2a29a69a3353425be40c50cfae
Subhadip757 <SubhadipGitHub@gmail.com>
2024-02-19 12:57:13 +0530

    new file

---
+++ cssStyes/src/App.jsx
@@ -0,0 +1,20 @@
+
+//External CSS
+//Modules CSS
+//INLINE CSS
+
+
+
+import Button from "./Button"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Button />
+    </>
+  )
+}
+
+export default App
---
+++ my-react-app/src/Header.jsx
@@ -0,0 +1,20 @@
+
+function Header(){
+    return (
+        <>
+            <h1>My Website</h1>
+            <nav>
+                <ul>
+                    <li> <a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                </ul>
+            </nav>
+            <hr />
+            <hr />
+        </>
+    );
+}
+
+export default Header
